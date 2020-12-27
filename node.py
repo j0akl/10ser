a93c35c7c24e920e4ca60180141ee82ed16a0d25
Jake Lynn <jklynn9@gmail.com>
2020-12-26 21:51:03 -0500

    generates network w random weights and biases

--- node.py
+++ node.py
@@ -5,15 +5,16 @@ import numpy as np
 TODO:
     - need to add support for multiple dimesions
     - optimize (after its working)
-    - 
+    - break into individual files
 """
 
 class Node:
 
     # inputs is an array of values
-    def __init__(self, input_nodes):
-        self.inputs = input_nodes
-        self.weights = np.random.rand(len(self.inputs))
+    def __init__(self, prev_layer):
+        self.inputs = np.zeros_like(len(prev_layer))
+        # size will have to be changed w multidimensional inputs
+        self.weights = np.random.rand(len(prev_layer))
         self.bias = random.random()
 
     def update_inputs(self, inputs):
@@ -21,7 +22,7 @@ class Node:
 
     def get_value(self):
         value = 0
-        for i in range(len(self.inputs)):
+        for i in range(self.inputs.size):
             value += self.inputs[i]*self.weights[i]
         return value + self.bias
 
@@ -42,14 +43,18 @@ class Node:
 class Layer:
 
     # nodes is a list of nodes
-    def __init__(self, length):
-        self.nodes = [Node([]))]
+    def __init__(self, length, prev_layer):
+        self.prev = prev_layer
+        self.nodes = [Node(self.prev) for i in range(length)]
 
-    def pass_inputs_to_nodes(self, prev_layer):
+    def update_prev(self, new_prev):
+        self.prev = new_prev
+
+    def pass_inputs_to_nodes(self):
         # this can be updated for speed later, just focus on concept atm
         # prev_layer is the gen_outputs of the prev layer
         for i in range(len(nodes)):
-                nodes[i].update_inputs(prev_layer)
+                nodes[i].update_inputs(self.prev)
 
     def generate_outputs(self):
         values = []
@@ -63,16 +68,19 @@ class Network:
         # layers should be an array of integers
         # length of layers is num layers, value at i
         # is the number of neurons in that layer
-        self.layers = [Layer(num_nodes) for num_nodes in layers]
-
-
-
-
-
-
+        self.input_layer = np.zeros_like(layers[0])
+        self.layers = []
+        for i in range(len(layers)):
+            if i != 0:
+                self.layers.append(Layer(layers[i],
+                                         np.zeros_like(range(layers[i-1]))))
 
 
 
 
 if __name__ == "__main__":
-    node = Node([1, 2, 3])
+    net = Network([2, 3, 4, 1])
+    for i in net.layers:
+        for j in i.nodes:
+            print(j.weights)
+
