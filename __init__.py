7b269e0a81d5b5469d3d9724cc5f930b6e61fa25
Jake Lynn <jklynn9@gmail.com>
2020-12-27 16:55:18 -0500

    removed main.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-import random
-import numpy as np
-
-from network.network import Network
-
-if __name__ == "__main__":
-    net = Network([10, 5, 6, 1])
-    net([1, 2, 3, 4, 5, 6, 7,  9, 0])
-    net.layers_()
