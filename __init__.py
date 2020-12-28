37714e9305cb62d85fe6471c434c37ad2cccd580
Jake Lynn <jklynn9@gmail.com>
2020-12-28 15:58:54 -0500

    removed tensors

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 import numpy as np
 from _10ser.layer import Linear, ReLU
-from _10ser.tensor import Tensor
 from _10ser.dataset import Dataset
 from _10ser.net import Network
 from _10ser.loss import MSELoss
@@ -8,16 +7,16 @@ from _10ser.loss import MSELoss
 
 
 if __name__ == "__main__":
-    test = Tensor([1])
-    x = Tensor([i for i in range(100)])
-    y = Tensor([i for i in range(100)])
+    test = np.array([1])
+    x = np.array([i for i in range(100)])
+    y = np.array([i*2 for i in range(100)])
 
-    net = Network([Linear(1, 1, activation_fn=ReLU()), Linear(1, 1, activation_fn=ReLU())], loss=MSELoss())
-    print("attempt 1: ", net(Tensor([1])))
+    net = Network([Linear(1, 1, activation_fn=ReLU())], loss=MSELoss())
+    print("attempt 1: ", net(np.array([1])))
     ds = Dataset(x, y)
     x_ds, y_ds = ds.create(shuffle=False, split=0.8)
-    net.train(x_ds, 20, 2, .0005, y_ds)
-    print("attempt 1: ", net(Tensor([20])))
-    print("attempt 2: ", net(Tensor([2])))
-    print("attempt 3: ", net(Tensor([3])))
-    print("attempt 4: ", net(Tensor([4])))
+    print(x_ds)
+    net.train(x_ds, 1, 2, .1, y_ds)
+    for i in range(10):
+        print(y[i])
+        print("out: ", net(x[i]))
