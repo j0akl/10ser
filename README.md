42920036908085cab8f1eab5accc6366eb4011f7
Jake Lynn <jklynn9@gmail.com>
2020-12-28 15:05:11 -0500

    initialized branch

--- README.md
+++ README.md
@@ -11,5 +11,9 @@ TODO:
  - array of arrays of weights with last element bias? first elem bias?
  - another method should load weights and biases
 
+Thinking my representation isnt great, might have to redo that.
+Better to represent the weights and biases in the network as a single set of
+arrays rather than store them in layer objects?
+
 
 
--- __init__.py
+++ __init__.py
@@ -9,16 +9,15 @@ from _10ser.loss import MSELoss
 
 if __name__ == "__main__":
     test = Tensor([1])
-    x = Tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    y = Tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    x = Tensor([i for i in range(100)])
+    y = Tensor([i for i in range(100)])
 
     net = Network([Linear(1, 1, activation_fn=ReLU()), Linear(1, 1, activation_fn=ReLU())], loss=MSELoss())
     print("attempt 1: ", net(Tensor([1])))
     ds = Dataset(x, y)
     x_ds, y_ds = ds.create(shuffle=False, split=0.8)
-    net.train(x_ds, 50, 5, .1, y_ds)
+    net.train(x_ds, 20, 2, .0005, y_ds)
     print("attempt 1: ", net(Tensor([20])))
     print("attempt 2: ", net(Tensor([2])))
     print("attempt 3: ", net(Tensor([3])))
     print("attempt 4: ", net(Tensor([4])))
-
